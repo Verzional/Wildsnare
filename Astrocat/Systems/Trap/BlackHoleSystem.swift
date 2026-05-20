82c7c75cccf61bae2719867a443799e8a7c2c2dc
Verzional <verzional777@gmail.com>
2026-05-20 12:03:52 +0700

    feat: integrate Player and Trap sound effects

--- Astrocat/Systems/Trap/BlackHoleSystem.swift
+++ Astrocat/Systems/Trap/BlackHoleSystem.swift
@@ -6,13 +6,35 @@
 //
 
 import GameplayKit
+import SpriteKit
 
 class BlackHoleSystem: GKComponent {
+    private var audioNode: SKAudioNode?
+    
+    override func didAddToEntity() {
+        super.didAddToEntity()
+        
+        if let url = Bundle.main.url(forResource: "BlackHole", withExtension: "mp3"),
+           let trapNode = entity?.component(ofType: GKSKNodeComponent.self)?.node {
+            let sound = SKAudioNode(url: url)
+            sound.name = "blackHoleSound"
+            sound.autoplayLooped = true
+            sound.run(SKAction.changeVolume(to: 0.0, duration: 0))
+            audioNode = sound
+            
+            trapNode.addChild(sound)
+            sound.run(SKAction.play())
+        }
+    }
+
     override func update(deltaTime seconds: TimeInterval) {
         guard let trapData = entity?.component(ofType: TrapComponent.self),
               trapData.type == .blackHole,
               !trapData.isOnCooldown
-        else { return }
+        else {
+            audioNode?.run(SKAction.changeVolume(to: 0.0, duration: 0.2))
+            return 
+        }
         
         guard let trapNode = entity?.component(ofType: GKSKNodeComponent.self)?.node,
               let scene = trapNode.scene as? GameScene,
@@ -27,6 +49,11 @@ class BlackHoleSystem: GKComponent {
             let forceVector = CGVector(dx: (dx / distance) * trapData.pullForce,
                                        dy: (dy / distance) * trapData.pullForce)
             playerNode.physicsBody?.applyForce(forceVector)
+            
+            let volume = Float(1.0 - (distance / trapData.radius))
+            audioNode?.run(SKAction.changeVolume(to: volume, duration: 0.1))
+        } else {
+            audioNode?.run(SKAction.changeVolume(to: 0.0, duration: 0.2))
         }
     }
 }
